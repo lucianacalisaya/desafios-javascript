07fa24204337f5d508ee59c5b83f2ec3c4028173
lucianacalisaya <73263531+lucianacalisaya@users.noreply.github.com>
2021-10-20 14:35:14 -0300

    Desafio 6 obligatorio

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
-//DESAFIO 4
-/*
-function calcular(plataformaUno, plataformaDos, plataformaTres, plataformaCuatro, plataformaCinco, plataformaSeis, pelicula) {
+let listaDePlataformas = ["Netflix", "HBO Max", "Disney+", "Star+", "Paramount", "Amazon Prime"]
+
+function Pelicula(titulo, genero, duracion, director, elenco, plataforma) {
+    this.titulo = titulo;
+    this.genero = genero;
+    this.duracion = duracion;
+    this.director = director;
+    this.elenco = elenco;
+    this.plataforma = plataforma;
+}
+
+const pelicula1 = new Pelicula("The Avengers", "Accion, Ciencia ficcion", "2:23 hs", "Joss Whedon", "Robert Downey Jr., Chris Evans, Jeremy Renner, Mark Ruffalo, Chris Hemsworth, Scarlett Johansson, Tom Hiddleston", listaDePlataformas[0] + ", " + listaDePlataformas[1] + ", " + listaDePlataformas[2] + " y " + listaDePlataformas[5]);
+
+const pelicula2 = new Pelicula("Mean Girls", "Comedia", "1:37 hs", "Mark Waters", "Lindsay Lohan, Rachel McAdams, Tim Meadows, Amy Poehler, Ana Gasteyer, Lacey Chabert, Lizzy Caplan", listaDePlataformas[5]);
+
+const pelicula3 = new Pelicula("The Hobbit: An Unexpected Journey", "Aventura, Fantasia epica", "2:49 hs", "Peter Jackson", "Ian Mckellen, Martin Freeman, Richard Armitage, James Nesbitt, Ken Stott, Sylvester McCoy, Barry Humphries", listaDePlataformas[1]);
+
+const pelicula4 = new Pelicula("Charlie and the Chocolate Factory", "Aventuras, Comedia", "1:55 hs", "Tim Burton", "Johnny Depp, Freddie Highmore, David Kelly, Helena Bonham Carter, Noah Taylor, Missi Pyle, James Fox", listaDePlataformas[5]);
+
+function calcular(pelicula) {
     switch (pelicula) {
-        case "The Avengers":
-            return plataformaUno + " y " + plataformaDos;
+        case pelicula1.titulo:
+            return pelicula1.plataforma;
             break;
-        case "Mean Girls":
-            return plataformaUno + " y " + plataformaTres;
+        case pelicula2.titulo:
+            return pelicula2.plataforma;
             break;
-        case "The Hobbit: An Unexpected journey":
-            return plataformaDos + "," + plataformaTres + " y " + plataformaCuatro;
+        case pelicula3.titulo:
+            return pelicula3.plataforma;
             break;
-        case "Charlie y la fabrica de chocolate":
-            return plataformaCinco + " y " + plataformaSeis;
+        case pelicula4.titulo:
+            return pelicula4.plataforma;
             break;
         default:
             return alert("La pelicula no fue encontrada en ninguna plataforma");
@@ -20,35 +37,19 @@ function calcular(plataformaUno, plataformaDos, plataformaTres, plataformaCuatro
     }
 }
 
-let plataformaUno = "Netflix";
-let plataformaDos = "HBO Max";
-let plataformaTres = "Disney+";
-let plataformaCuatro = "Star+";
-let plataformaCinco = "Paramount";
-let plataformaSeis = "Amazon Prime";
-let pelicula = prompt("Ingrese la pelicula que desea buscar");
-let resultado = calcular(plataformaUno, plataformaDos, plataformaTres, plataformaCuatro, plataformaCinco, plataformaSeis, pelicula);
-alert("La pelicula " + pelicula + " esta en: " + resultado);
-*/
 
-//DESAFIO 5
-function Pelicula(titulo, genero, duracion, director, elenco) {
-    this.titulo = titulo;
-    this.genero = genero;
-    this.duracion = duracion;
-    this.director = director;
-    this.elenco = elenco;
-    this.informar = function() { console.log("Datos de la pelicula " + this.titulo + "\nGénero: " + this.genero + "\nDuración: " + this.duracion + "\nDirector: " + this.director + "\nElenco: " + this.elenco); }
+let pelicula = prompt("Ingrese la pelicula que desea buscar");
+let resultado = calcular(pelicula);
+alert("La pelicula " + pelicula + " esta en: " + resultado);
+
+
+let listaDePeliculas = [pelicula1, pelicula2, pelicula3, pelicula4];
+
+for (const pelicula of listaDePeliculas) {
+    console.log(pelicula.titulo);
 }
 
-const pelicula1 = new Pelicula("The Avengers", "Accion, Ciencia ficcion", "2:23 hs", "Joss Whedon", "Robert Downey Jr., Chris Evans, Jeremy Renner, Mark Ruffalo, Chris Hemsworth, Scarlett Johansson, Tom Hiddleston");
-pelicula1.informar();
 
-const pelicula2 = new Pelicula("Mean Girls", "Comedia", "1:37 hs", "Mark Waters", "Lindsay Lohan, Rachel McAdams, Tim Meadows, Amy Poehler, Ana Gasteyer, Lacey Chabert, Lizzy Caplan");
-pelicula2.informar();
-
-const pelicula3 = new Pelicula("The Hobbit: An Unexpected Journey", "Aventura, Fantasia epica", "2:49 hs", "Peter Jackson", "Ian Mckellen, Martin Freeman, Richard Armitage, James Nesbitt, Ken Stott, Sylvester McCoy, Barry Humphries");
-pelicula3.informar();
-
-const pelicula4 = new Pelicula("Charlie and the Chocolate Factory", "Aventuras, Comedia", "1:55 hs", "Tim Burton", "Johnny Depp, Freddie Highmore, David Kelly, Helena Bonham Carter, Noah Taylor, Missi Pyle, James Fox");
-pelicula4.informar();
+//Complementario buscar sort
+const mayorCantidadDePlataformas = listaDePeliculas.filter(pelicula => pelicula.plataforma.length < 100);
+console.log(mayorCantidadDePlataformas)
