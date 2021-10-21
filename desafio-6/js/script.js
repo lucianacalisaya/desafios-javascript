c4f7aa6eba8b1287f0e1487256b991143beffd30
lucianacalisaya <73263531+lucianacalisaya@users.noreply.github.com>
2021-10-21 00:51:05 -0300

    Probando el sort de nuevo

--- desafio-6/js/script.js
+++ desafio-6/js/script.js
@@ -1,6 +1,14 @@
-let listaDePlataformas = ["Netflix", "HBO Max", "Disney+", "Star+", "Paramount", "Amazon Prime"]
+let listaDePlataformas = ["Netflix", "HBO Max", "Disney+", "Star+", "Paramount", "Amazon Prime"];
+
+console.log(listaDePlataformas[3]);
+
 listaDePlataformas.sort();
+
+console.log(listaDePlataformas[3]);
+
 console.log(listaDePlataformas);
+
+/*BORRAR SORT DESPUES PORQUE ME CAMBIA EL ORDEN DE LAS PLATAFORMAS*/
 
 function Pelicula(titulo, genero, duracion, director, elenco, plataforma) {
     this.titulo = titulo;
@@ -11,13 +19,13 @@ function Pelicula(titulo, genero, duracion, director, elenco, plataforma) {
     this.plataforma = plataforma;
 }
 
-const pelicula1 = new Pelicula("The Avengers", "Accion, Ciencia ficcion", "2:23 hs", "Joss Whedon", "Robert Downey Jr., Chris Evans, Jeremy Renner, Mark Ruffalo, Chris Hemsworth, Scarlett Johansson, Tom Hiddleston", listaDePlataformas[0] + ", " + listaDePlataformas[1] + ", " + listaDePlataformas[2] + " y " + listaDePlataformas[5]);
+const pelicula1 = new Pelicula("The Avengers", "Accion, Ciencia ficcion", 143, "Joss Whedon", "Robert Downey Jr., Chris Evans, Jeremy Renner, Mark Ruffalo, Chris Hemsworth, Scarlett Johansson, Tom Hiddleston", listaDePlataformas[0] + ", " + listaDePlataformas[1] + ", " + listaDePlataformas[2] + " y " + listaDePlataformas[5]);
 
-const pelicula2 = new Pelicula("Mean Girls", "Comedia", "1:37 hs", "Mark Waters", "Lindsay Lohan, Rachel McAdams, Tim Meadows, Amy Poehler, Ana Gasteyer, Lacey Chabert, Lizzy Caplan", listaDePlataformas[5]);
+const pelicula2 = new Pelicula("Mean Girls", "Comedia", 97 + "hs", "Mark Waters", "Lindsay Lohan, Rachel McAdams, Tim Meadows, Amy Poehler, Ana Gasteyer, Lacey Chabert, Lizzy Caplan", listaDePlataformas[5]);
 
-const pelicula3 = new Pelicula("The Hobbit: An Unexpected Journey", "Aventura, Fantasia epica", "2:49 hs", "Peter Jackson", "Ian Mckellen, Martin Freeman, Richard Armitage, James Nesbitt, Ken Stott, Sylvester McCoy, Barry Humphries", listaDePlataformas[1]);
+const pelicula3 = new Pelicula("The Hobbit: An Unexpected Journey", "Aventura, Fantasia epica", 169, "Peter Jackson", "Ian Mckellen, Martin Freeman, Richard Armitage, James Nesbitt, Ken Stott, Sylvester McCoy, Barry Humphries", listaDePlataformas[1]);
 
-const pelicula4 = new Pelicula("Charlie and the Chocolate Factory", "Aventuras, Comedia", "1:55 hs", "Tim Burton", "Johnny Depp, Freddie Highmore, David Kelly, Helena Bonham Carter, Noah Taylor, Missi Pyle, James Fox", listaDePlataformas[5]);
+const pelicula4 = new Pelicula("Charlie and the Chocolate Factory", "Aventuras, Comedia", 115, "Tim Burton", "Johnny Depp, Freddie Highmore, David Kelly, Helena Bonham Carter, Noah Taylor, Missi Pyle, James Fox", listaDePlataformas[5]);
 
 function calcular(pelicula) {
     switch (pelicula) {
@@ -46,6 +54,9 @@ alert("La pelicula " + pelicula + " esta en: " + resultado);
 
 let listaDePeliculas = [pelicula1, pelicula2, pelicula3, pelicula4];
 
-for (const pelicula of listaDePeliculas) {
-    console.log(pelicula.titulo);
-}
+const pelicula5 = new Pelicula("Probando", "Probando", "Probando", "Probando", "Probando", "Probando");
+listaDePeliculas.push(pelicula5);
+
+const resultadoBusqueda = listaDePeliculas.find(pelicula => pelicula.plataforma === listaDePlataformas[5]);
+
+console.log(resultadoBusqueda.titulo);
