2bedd2cf10412af9501915314b89048ffa9e9af5
lucianacalisaya <73263531+lucianacalisaya@users.noreply.github.com>
2021-11-22 13:07:45 -0300

    ordenando el script (Desafio 14 y 3er Preentrega)

--- desafio-14/js/script.js
+++ desafio-14/js/script.js
@@ -1,16 +1,17 @@
 //RENDERIZAR
 $(document).ready(function() {
-    // loadingMovies();
-    // moviesInLibrary();
+    //No estoy segura si tengo que poner todo el get json aca dentro
+});
 
-    const URLJSON = "js/movies.json";
+const URLJSON = "js/movies.json";
 
-    $.getJSON(URLJSON, function(respuesta, estado) {
-        if (estado == "success") {
-            let movies = respuesta.movies;
-            for (const movie of movies) {
-                //<img class="card__poster--image" src="${movie.poster} alt="Poster de ${movie.title}">
-                $("#movie").append(`<div class="movie__item">
+$.getJSON(URLJSON, function(respuesta, estado) {
+    if (estado == "success") {
+        let movies = respuesta.movies;
+        for (const movie of movies) {
+            //<img class="card__poster--image" src="${movie.poster} alt="Poster de ${movie.title}">
+            //agregar plataformas
+            $("#movie").append(`<div class="movie__item">
                     <div class="movie__favorite">
                         <button class="movie__favorite--button" id="btn${movie.id}"><i class="movie__favorite--icon fas fa-ticket-alt"></i></button>
                     </div>
@@ -24,45 +25,15 @@ $(document).ready(function() {
                         </ul>
                     </div>
             </div>`); //agregar director y elenco con un +
-                $(`#btn${movie.id}`).on('click', function() {
-                    addToLibrary(movie);
-                });
-            };
-        }
-    });
+            $(`#btn${movie.id}`).on('click', function() {
+                addToLibrary(movie);
+            });
+        };
+    }
 });
 
 let library = [];
 
-
-
-
-//CARTILLA DE PELICULAS
-/*
-function loadingMovies() {
-    for (const movie of movies) {
-        //<img class="card__poster--image" src="${movie.poster} alt="Poster de ${movie.title}">
-        $("#movie").append(`<div class="movie__item">
-            <div class="movie__favorite">
-                <button class="movie__favorite--button" id="btn${movie.id}"><i class="movie__favorite--icon fas fa-ticket-alt"></i></button>
-            </div>
-            <div class="movie__poster">
-            </div>
-            <div class="movie__details">
-                <h4 class="movie__title">${movie.title}</h4>
-                <p class="movie__plataform">${movie.platform}</p>
-                <ul class="movie__list">
-                    <li class="movie__list--item"><span class="movie__list--bold">Genero:</span>${movie.genre}</li>
-                    <li class="movie__list--item"><span class="movie__list--bold">Duracion:</span>${movie.duration}</li>
-                </ul>
-            </div>
-    </div>`); //agregar director y elenco con un +
-        $(`#btn${movie.id}`).on('click', function() {
-            addToLibrary(movie);
-        });
-    };
-};*/
-//FIN CARTILLA DE PELICULAS
 
 //BUSCADOR DE PELICULAS
 let searchboxForm = document.getElementById("searchboxForm");
@@ -154,33 +125,3 @@ function buscar() {
 };
 
 //FIN BUSCADOR CON SWAL
-
-//ICONO LIBRERIA - A resolver
-// let heart = document.getElementById("heart");
-
-// function guardarPelicula() {
-//     heart.style.border = "2px solid red";
-//     heart.style.background = "red";
-// }
-
-//FIN LIBRERIA DE PELICULAS
-
-//JSON
-
-// searchboxForm.addEventListener("submit", guardarBusqueda);
-
-// function guardarBusqueda(ev) {
-//     let busquedasRecientes = [searchboxText.value];
-//     const busquedasAJSON = JSON.stringify(busquedasRecientes);
-//     localStorage.setItem("peliculasBuscadas", busquedasAJSON);
-//     ev.preventDefault();
-
-// };
-// localStorage.setItem("peliculasBuscadas", "myValue");
-
-// let peliculaBuscada = localStorage.getItem("peliculasBuscadas");
-
-// // Check for changes in the local item and log them..
-// searchboxForm.addEventListener('storage', function(event) {
-//     console.log('The value for ' + event.key + ' was changed from' + event.oldValue + ' to ' + event.newValue);
-// }, false);
